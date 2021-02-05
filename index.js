5318a8c0090fdfdd66c3103c3ae7a040db4929be
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-02-05 18:39:49 -0300

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { showData } = require('./controller/repositoryController')
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
